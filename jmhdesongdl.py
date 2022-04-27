cf638f483186e3bd6c27290d4b2ddafe9f6ae3c2
VicCodezz <viccodezz@outlook.com>
2022-04-26 22:44:01 -0400

    Switch to YouTubeDL following Pytube issues

--- jmhdesongdl.py
+++ jmhdesongdl.py
@@ -1,3 +1,4 @@
+import youtube_dl
 import urllib.request
 import re
 from time import sleep
@@ -46,6 +47,7 @@ def removeIllegal(illegal):
     return illegal
 
 
+
 for i in range(0, len(songs)): #For Loop
     songName = songs[i] #Gets Current Song Name
     songName = songName.replace("\n", "") #Removes Linebreak in song name
@@ -53,39 +55,25 @@ for i in range(0, len(songs)): #For Loop
     urlName = urlName.replace("\n", "")
 
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + urlName) #Gets YouTube Link
-    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode()) #Gets Video IDs
-    video_ids = "https://www.youtube.com/watch?v=" + video_ids[0] #Gets The ID of the First Video
-    print(video_ids) #Prints Song Name
+    video_id = re.findall(r"watch\?v=(\S{11})", html.read().decode()) #Gets Video IDs As List
+    video_id = video_id[0] #Gets The ID of the First Video
+    vidurl = "https://www.youtube.com/watch?v=" + video_id
+    del(video_id)
+    del(html)
+    del(urlName)
 
-    #Stuff for Downloading as MP4
-    from pytube import YouTube
-    yt = YouTube(video_ids)
-    fileName = songName + ".mp4"
-    songName = yt.title
-    ys = yt.streams.get_by_itag('22')
-    ys.download(filename= fileName)
+    vidinfo = youtube_dl.YoutubeDL().extract_info(
+        url = vidurl,download=False
+    )
+
+    fname = f"{vidinfo['title']}.mp3"
+    o ={
+
+        'format':'bestaudio/best',
+        'keepvideo':False,
+        'outtmpl':fname
+    }
+
+    with youtube_dl.YoutubeDL(o) as ydl:
+        ydl.download([vidinfo['webpage_url']])
     
-    songName = removeIllegal(songName)
-
-    #Convert To MP3
-    from moviepy.editor import *
-    mp4vid = fileName
-    mp3exp = songName + ".mp3"
-    videoclip = VideoFileClip(mp4vid)
-    audioclip = videoclip.audio
-    audioclip.write_audiofile(mp3exp)
-    audioclip.close()
-    videoclip.close()
-
-
-#Delete MP4 and "v" files
-import os
-
-dir_name = "."
-test = os.listdir(dir_name)
-
-for item in test:
-    if item.endswith(".mp4"):
-        os.remove(os.path.join(dir_name, item))
-
-os.remove("v")
